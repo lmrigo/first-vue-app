108eb89788c00f309d187863d63006813f7d304f
Lucas Rigo <lucas.rigo@sap.com>
2024-05-27 16:10:08 -0300

    Modules 1 and 2

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="card">
+    <div id="header">
+      <slot name="header"></slot>
+    </div>
+    <div id="content">
+      <slot></slot>
+    </div>
+    <div id="footer">
+      <slot name="footer"></slot>
+    </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'card'
+}
+
+</script>
+
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  padding: 8px;
+  margin: 10px;
+  width: 400px;
+}
+
+</style>
---
+++ src/components/Popup.vue
@@ -0,0 +1,12 @@
+<template>
+  <h3>This is popup.</h3>
+  <button @click="$emit('close')">Close</button>
+</template>
+
+<script>
+
+export default {
+  name: 'Popup',
+  emits: ['close']
+}
+</script>
